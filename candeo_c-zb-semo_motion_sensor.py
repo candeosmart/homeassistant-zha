4dfbd17daf7e17e6d717a2fcc2c3e25bf17b552a
martyn-vesternet <development@candeo.io>
2025-07-10 09:46:38 +0100

    Update candeo_c-zb-semo_motion_sensor.py
    Adds illuminance calibration to more accurately reflect real world readings

--- candeo_c-zb-semo_motion_sensor.py
+++ candeo_c-zb-semo_motion_sensor.py
@@ -1,5 +1,6 @@
 """Candeo C-ZB-SEMO Motion Sensor."""
 
+import math
 from zigpy.profiles import zha
 from zigpy.quirks import CustomCluster, CustomDevice
 from zigpy.zcl.clusters.general import (
@@ -25,6 +26,30 @@ class CandeoIasZone(CustomCluster, IasZone):
     """Candeo IasZone cluster."""
 
     _CONSTANT_ATTRIBUTES = {ZONE_TYPE: IasZone.ZoneType.Motion_Sensor}
+
+class CandeoIlluminanceMeasurementCluster(IlluminanceMeasurement, CustomCluster):
+    """Candeo Illuminance Measurement Cluster."""
+
+    def _update_attribute(self, attrid, value):
+        if attrid == self.AttributeDefs.measured_value.id:
+            value = pow(10, ((value - 1) / 10000))
+            value = self.lux_calibration(value)
+            value = 10000 * math.log10(value) + 1
+            value = round(value)
+        super()._update_attribute(attrid, value)
+
+    @staticmethod
+    def lux_calibration(value):
+        """Calibrate lux reading from device."""
+        lux_value = 1
+        if 0 < value <= 2200:
+            lux_value = -7.969192 + (0.0151988 * value)
+        elif 2200 < value <= 2500:
+            lux_value = -1069.189434 + (0.4950663 * value)
+        elif value > 2500:
+            lux_value = (78029.21628 - (61.73575 * value)) + (0.01223567 * (value**2))
+        lux_value = max(lux_value, 1)
+        return lux_value
 
 class CandeoCZBSEMOMotionSensor(CustomDevice):
     """Candeo C-ZB-SEMO Motion Sensor."""
@@ -55,7 +80,7 @@ class CandeoCZBSEMOMotionSensor(CustomDevice):
                 INPUT_CLUSTERS: [
                     Basic.cluster_id,
                     PowerConfiguration.cluster_id,
-                    IlluminanceMeasurement.cluster_id,
+                    CandeoIlluminanceMeasurementCluster,
                     CandeoIasZone,
                 ],
                 OUTPUT_CLUSTERS: [ ],
